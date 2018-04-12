5d294b625ff22d4c24f5d73511712fe4d185d58b
Riwert <riwert@gmail.com>
2018-04-12 19:23:53 +0200

    clear whitespaces

--- posts/models.py
+++ posts/models.py
@@ -6,6 +6,6 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     created_at = models.DateTimeField(default=datetime.now, blank=True)
-    
+
     def __str__(self):
         return self.title
--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [    
+urlpatterns = [
     path('', views.index, name="index"),
     path('<int:id>', views.show, name="show"),
 ]
